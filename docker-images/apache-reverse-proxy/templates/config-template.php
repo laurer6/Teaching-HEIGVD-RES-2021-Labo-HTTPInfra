a33c9855eb27c236c6ab39538488f5dcee3b76b7
ggeggc <ggeggc@hotmail.fr>
2021-05-29 20:01:12 +0200

    modified startup script to inject ENV variables in PHP tempalte and dynamically configure apache (RP)

---
+++ docker-images/apache-reverse-proxy/templates/config-template.php
@@ -0,0 +1,16 @@
+<?php
+    $static_app = getenv('STATIC_APP');
+    $dynamic_app = getenv('DYNAMIC_APP');
+?>
+
+<VirtualHost *:80>
+	ServerName demo.res.ch
+		
+	ProxyPass '/api/students/' 'http://<?php print $dynamic_app; ?>/'
+	ProxyPassReverse '/api/students/' 'http://<?php print $dynamic_app; ?>/'
+	
+	ProxyPass '/' 'http://<?php print $static_app; ?>/'
+	ProxyPassReverse '/' 'http://<?php print $static_app; ?>/'
+		
+</VirtualHost>
+
